ef06bb4e8939b8b85175bc72955b044b0d97055b
croftkie <croftkie@proton.me>
2024-01-31 23:33:41 +0000

    tetris local build pushed

---
+++ Position.cs
@@ -0,0 +1,14 @@
+namespace tetris
+{
+    public class Position
+    {
+        public int Row {get; set;}
+        public int Column {get; set;}
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
